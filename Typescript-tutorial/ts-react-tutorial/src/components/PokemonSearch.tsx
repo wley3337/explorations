cef9bd85cd999449e313d363a9f0e82d15a35905
wley3337 <wley3337@gmail.com>
2019-09-15 15:25:33 -0400

    added typescript tutorial and git ignore for graphql exercise files"

--- Typescript-tutorial/ts-react-tutorial/src/components/PokemonSearch.tsx
+++ Typescript-tutorial/ts-react-tutorial/src/components/PokemonSearch.tsx
@@ -5,24 +5,32 @@ import { number } from 'prop-types';
 
 
 interface SearchState{
-    name: string,
-    numberOfAbilities: number,
-    baseExperience: number,
-    imageUrl: string,
+   pokemon: Pokemon,
     error: boolean
 }
 
-export class PokemonSearch extends Component<User, SearchState> {
-    pokemonRef: React.RefObject<HTMLInputElement>
+interface Pokemon{
+    name: string,
+    numberOfAbilities: number,
+    baseExperience: number,
+    imageUrl: string
+}
 
+interface Stuff{
+    name: string,
+    age: number,
+    loggedIn: boolean
+}
+const foo: (string|number|boolean)[] = [1, "this", 32, 233, "that", true]
+
+export class PokemonSearch extends Component<User, SearchState, Stuff> {
+    pokemonRef: React.RefObject<HTMLInputElement>
+    
     constructor(props: User ){
         super(props)
         this.state = {
             error: false,
-            name: '',
-            numberOfAbilities: 0,
-            baseExperience: 0,
-            imageUrl: ''
+            pokemon: null
         }
         this.pokemonRef = React.createRef();
     }
@@ -31,24 +39,45 @@ export class PokemonSearch extends Component<User, SearchState> {
         fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}`)
         .then( res => {
             if(res.status !== 200){
-                this.setState({
-                    error: true
-                })
+                this.setState({error: true });
+                return
             }
+            res.json().then(data =>{
+                this.setState({
+                    error: false,
+                    pokemon: {
+                        name: data.name,
+                        numberOfAbilities: data.abilities.length,
+                        baseExperience: data.base_experience,
+                        imageUrl: data.sprites.front_default 
+                    }
+                })
+            })
         })
-
-
-        console.log('clicked')
     }
-
+    
     render() {
-        const { name, numberOfPokemons} = this.props;
-
+        const bar: Stuff = { name: "this", age: 25, loggedIn: true}
+        const { name: userName, numberOfPokemons} = this.props;
+        const {error, pokemon } = this.state;
+        let resultMarkup;
+        
+        if(error){
+            resultMarkup = <p>Pokemon not found please try again</p>
+        }else if(this.state.pokemon){
+            resultMarkup = <div>
+                <img src={pokemon.imageUrl} alt="pokemon" className="pokemon-image"/>
+                <p>{pokemon.name} has {pokemon.numberOfAbilities} has base experience {pokemon.baseExperience} </p>
+            </div>
+        }
+        
         return (
             <div>
-                <p>User {name} {numberOfPokemons && <span>has { numberOfPokemons } pokemons</span>} </p>
+            {bar}
+                <p>User {userName}{' '} {numberOfPokemons && <span>has { numberOfPokemons } pokemons</span>} </p>
                 <input type="text" ref={this.pokemonRef}/>
                 <button onClick={this.onSearchClick}>Search</button>
+                {resultMarkup}
             </div>
         )
     }
